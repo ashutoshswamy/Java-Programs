a345cf6aa363dd1bb367963d998161a95d4dc00e
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-10 01:17:25 -0700

    Add files via upload

---
+++ While Loops/FiniteWhileLoop.java
@@ -0,0 +1,10 @@
+public class FiniteWhileLoop {
+    public static void main(String[] args) {
+        int number = 1;
+        while(number <= 10){
+            System.out.println();
+            number++;
+        }
+        System.out.println("This is the end");
+    }    
+}
---
+++ While Loops/InfiniteWhileLoop.java
@@ -0,0 +1,9 @@
+public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        int number = 10;
+        while(number > 1){
+            System.out.println();
+            number++;
+        }
+    }   
+}
