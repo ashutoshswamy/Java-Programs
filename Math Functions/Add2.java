63c13fce5a6d1ce6115b6a729a7325b8816715d5
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 01:17:22 -0700

    Add files via upload

--- Math Functions/Add2.java
+++ Math Functions/Add2.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Add2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int x, y;
+        int firstNumber, secondNumber;
         System.out.print("Please enter the first number: ");
-        x = scan.nextInt();
+        firstNumber= scan.nextInt();
         System.out.print("Please enter the second number: ");
-        y = scan.nextInt();
-        System.out.println(x + y);
+        secondNumber= scan.nextInt();
+        System.out.println(firstNumber + secondNumber);
     }
 }
