b9dc2573bb97273cd27c6465bd06558822d3d89d
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 19:24:09 +0530

    Update FloydsTriangle.java

--- For Loops/FloydsTriangle.java
+++ For Loops/FloydsTriangle.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class FloydsTriangle {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int counter;
+        System.out.print("Please enter the number of lines: ");
+        counter = scan.nextInt();
         int number = 1;
         //outer loop
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= counter; i++){
             //inner loop
             for(int j = 1; j <= i; j++){
                 System.out.print(number+" ");
