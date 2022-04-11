e6f16a37ab35a4894500417e4f017b863de96a85
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 19:24:37 +0530

    Update Add2.java

--- Math Functions/Add2.java
+++ Math Functions/Add2.java
@@ -7,7 +7,7 @@ public class Add2 {
         System.out.print("Please enter the first number: ");
         firstNumber = scan.nextInt();
         System.out.print("Please enter the second number: ");
-        secondNumber= scan.nextInt();
+        secondNumber = scan.nextInt();
         System.out.println(firstNumber + secondNumber);
     }
 }
