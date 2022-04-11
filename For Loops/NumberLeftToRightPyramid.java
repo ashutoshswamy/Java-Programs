db504a7c024353294241e90bf150acb35b254c83
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 21:06:08 +0530

    Update NumberLeftToRightPyramid.java

--- For Loops/NumberLeftToRightPyramid.java
+++ For Loops/NumberLeftToRightPyramid.java
@@ -1,7 +1,13 @@
-public class NumberLeftToRightPyramid {
+import java.util.Scanner;
+
+public class NumberPyramid1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the number of lines: ");
+        int counter;
+        counter = scan.nextInt();
         //outer loop
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= counter; i++){
             //inner loop
             for(int j = 1; j <= i; j++){
                 System.out.print(j+" ");
