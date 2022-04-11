61caf084058cde23bb887fea9cabde98809cdf47
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 19:23:21 +0530

    Update LeftToRightPyramid.java

--- For Loops/LeftToRightPyramid.java
+++ For Loops/LeftToRightPyramid.java
@@ -1,7 +1,13 @@
-public class LeftToRightPyramid {
+import java.util.Scanner;
+
+public class Pyramid1 { //Left to Right
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int counter;
+        System.out.print("Please enter the number of lines: ");
+        counter = scan.nextInt();
         //outer loop
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= counter; i++){
             //inner loop
             for(int j = 1; j <= i; j++){
                 System.out.print("*");
