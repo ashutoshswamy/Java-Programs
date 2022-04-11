3cc47a8d9659227a711034a72269f2b0002f4a30
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-04-11 09:25:53 -0700

    Add files via upload

---
+++ Do While Loops/FiniteDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class FiniteDoWhileLoop {
+    public static void main(String[] args) {
+        int x = 1;
+        do{
+            System.out.println(x);
+            x++;
+        } while(x <= 10);
+        System.out.println("This is the end");
+    }
+}
---
+++ Do While Loops/InfiniteDoWhileLoop.java
@@ -0,0 +1,9 @@
+public class InfiniteDoWhileLoop {
+    public static void main(String[] args) {
+        int x = 2;
+        do{
+            System.out.println(x);
+            x++;
+        } while(x > 1);
+    }
+}
